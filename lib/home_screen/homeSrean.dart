684a0e09cb2043e9aca0b0ac1cdc0a0d9c0cb77f
Admin <admin@192.168.0.173>
2022-10-29 22:40:45 -0700

    name changed

--- lib/home_screen/homeSrean.dart
+++ lib/home_screen/homeSrean.dart
@@ -24,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen>
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('WhatsApp'),
+          title: const Text('WhatsApp beta'),
           actions: [
             const Icon(Icons.search),
             PopupMenuButton(
